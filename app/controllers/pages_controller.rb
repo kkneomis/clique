ee62498334892680c06b33366d2ed4d6aab2a5e8
Simeon Kakpovi <skakpovi@gmail.com>
2015-04-03 21:37:17 +0000

    first

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+  
+  def shop
+  end
+  
+  def meet
+  end
+  
+  def join
+  end
+  
+  
+end
