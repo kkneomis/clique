f76b4781f1af20b85bc41e508877b022925c88ea
Simeon Kakpovi <skakpovi@gmail.com>
2015-05-10 05:22:47 +0000

    now?

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,7 +15,7 @@ class Order < ActiveRecord::Base
         :upload => 1,
         :invoice => id,
         :return => "#{Rails.application.secrets.app_host}#{return_url}",
-        :notify_url => "http://beencliqued.herokuapp.com/hook"
+        :notify_url => "#{Rails.application.secrets.app_host}/hook"
       }	
       values.merge!({ 
        "amount_1" => price,
